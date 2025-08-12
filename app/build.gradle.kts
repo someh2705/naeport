de168c088006d614324f4e42f3330c6f9eb09a0a
someh2705 <someh2705@gmail.com>
2025-08-13 00:42:36 +0900

    refactor(build): Relocate dependencies to convention plugin
    Move common dependencies from the main build.gradle.kts to the new AndroidConventionPlugin to centralize dependency management and improve build consistency across modules.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,12 +24,3 @@ android {
         }
     }
 }
-
-dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-}
--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -1,9 +1,11 @@
 import com.android.build.api.dsl.ApplicationExtension
+import com.someh.naeport.libs
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 
@@ -26,6 +28,12 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                 compileOptions {
                     sourceCompatibility = JavaVersion.VERSION_11
                     targetCompatibility = JavaVersion.VERSION_11
+                }
+
+                dependencies {
+                    "implementation"(libs.findBundle("default-dependencies").get())
+                    "testImplementation"(libs.findBundle("default-test-dependencies").get())
+                    "androidTestImplementation"(libs.findBundle("default-android-test-dependencies").get())
                 }
             }
 
